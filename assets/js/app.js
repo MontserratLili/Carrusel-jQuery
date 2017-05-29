03b6b44710c4f80c2e0e4670be012ef2db90bc21
MontserratLili <lauraliligm@gmail.com>
2017-05-29 16:56:23 -0500

    terminando funcionalidad bolitas

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,55 @@
-$(document).ready(function(){
-    
-    
-    
-    
-    
-    
-    
-    
-    
+$(document).ready(function () {
+
+    $("#ghost1").click(function () {
+        $("#fantasma1").show(0);
+        $("#fantasma2").hide(0);
+        $("#fantasma3").hide(0);
+        $("#fantasma4").hide(0);
+        $("#pacmanimg").hide(0);
+    });
+    $("#ghost2").click(function () {
+        $("#fantasma1").hide(0);
+        $("#fantasma2").show(0);
+        $("#fantasma3").hide(0);
+        $("#fantasma4").hide(0);
+        $("#pacmanimg").hide(0);
+    });
+    $("#ghost3").click(function () {
+        $("#fantasma1").hide(0);
+        $("#fantasma2").hide(0);
+        $("#fantasma3").show(0);
+        $("#fantasma4").hide(0);
+        $("#pacmanimg").hide(0);
+    });
+    $("#ghost4").click(function () {
+        $("#fantasma1").hide(0);
+        $("#fantasma2").hide(0);
+        $("#fantasma3").hide(0);
+        $("#fantasma4").show(0);
+        $("#pacmanimg").hide(0);
+    });
+     $("#pacman").click(function () {
+        $("#fantasma1").hide(0);
+        $("#fantasma2").hide(0);
+        $("#fantasma3").hide(0);
+        $("#fantasma4").hide(0);
+        $("#pacmanimg").show(0);
+    });
 });
+
+/*
+     $("#flip2").click(function(){
+         $("#panel").toggle(1000);
+    $("#panel2").show(1000);
+     });
+});
+
+$("button").click(function(){
+        $("#panel").animate({
+            left: '250px',
+            opacity: '0.5',
+            height: '150px',
+            width: '150px'
+        });
+    });*/
+
